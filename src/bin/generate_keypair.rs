c5f507341b8152d6ada201dabe9b95b80c3be1a8
keroroxx520@gmail.com <keroroxx520@gmail.com>
2025-05-09 10:37:18 +0800

    chore: generate public_key

--- src/bin/generate_keypair.rs
+++ src/bin/generate_keypair.rs
@@ -8,7 +8,13 @@ fn main() {
     let keypair = Keypair::new();
     let bytes = keypair.to_bytes();
     let json = serde_json::to_string(&bytes.to_vec()).unwrap();
-    let mut file = File::create("test-keypair.json").unwrap();
+    let mut file = File::create(".private_key").unwrap();
     file.write_all(json.as_bytes()).unwrap();
-    println!("Generated keypair with public key: {}", keypair.pubkey());
+    let mut file = File::create(".public_key").unwrap();
+    file.write_all(keypair.pubkey().to_string().as_bytes())
+        .unwrap();
+    println!(
+        "Generated keypair to .private_key and .public_key, public key: {}",
+        keypair.pubkey()
+    );
 }
